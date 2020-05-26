67aff5e6fe3283d1b69f211a06e60bb4b7093c85
dahalaazad <aazad.dahal@gmail.com>
2020-05-26 18:52:53 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,144 @@
+document.addEventListener ( 'DOMContentLoaded', () => {
+    for (let i = 0; i < 200; i++) {                         // Create 200 <div> elements
+        let div = document.createElement('div');                 
+        //div.id = i;  
+        document.querySelector('.grid').appendChild(div);
+    }
+
+    for (let i =0; i < 10; i++ ){
+        let div1 = document.createElement('div');
+        div1.classList.add('taken');
+        document.querySelector('.grid').appendChild(div1);
+    }
+    
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelectorAll('#score');
+    const StartBtn = document.querySelectorAll('#start-button');
+    const width = 10;
+    let timerId, nextRandom;
+
+    //The Tetrominoes
+    const lTetromino = [                                //0
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [                                        //1
+        [0,1,width+1,width+2],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ];
+
+    const tTetromino = [                                                    //2
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ];
+
+    const oTetromino = [                                //3
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ];
+
+    const iTetromino = [                                                    //4
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ];
+
+    /*const sTetromino = [                                                        //5
+        [width*2,width+1, width*2+1,width+2]
+    ]; */
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+    let random = Math.floor (Math.random() * theTetrominoes.length);
+    //console.log(randomTetromino);
+    
+    let current = theTetrominoes[random][currentRotation];
+    
+    //console.log(squares);
+    //console.log(current);
+    
+    //drawing up the 1st rotation in 1st tetromino
+
+    function draw () {
+        current.forEach (index => {
+             squares[currentPosition + index]
+            .classList
+            .add('tetromino')
+        })
+    }
+    
+    function removeDraw () {
+        current.forEach ( index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+    
+    
+
+    function freeze () {
+        if( current.some(
+            index => squares[currentPosition + index + width]
+                    .classList
+                    .contains('taken'))) {
+                        current
+                        .forEach(index => squares[currentPosition+index]
+                                         .classList.add('taken'))
+        nextRandom = Math.floor (Math.random() * theTetrominoes.length);
+        random = nextRandom;
+        current = theTetrominoes[random][currentRotation];
+        currentPosition = 4;
+        draw();
+        }
+    }
+    
+    
+    function moveDown () {
+        removeDraw()
+        currentPosition += width;
+        draw()
+        freeze()
+    }
+
+    timerId = setInterval(moveDown, 1000);
+
+    function moveLeft () {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    }
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
